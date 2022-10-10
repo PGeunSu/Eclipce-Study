41601e71ef63bd1f933fde228f4be07b6e18bb82
ParkGeunSu <geunsu902@gmail.com>
2022-10-10 23:38:00 +0900

    feat : 22.10.10 MySql
    DB최신화

--- MYSQL/22.10.10.sql
+++ MYSQL/22.10.10.sql
@@ -28,10 +28,12 @@ CREATE TABLE IF NOT EXISTS `board` (
   `hit` int DEFAULT NULL,
   `ip` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
--- 테이블 데이터 bookmarketdb.board:~0 rows (대략적) 내보내기
+-- 테이블 데이터 bookmarketdb.board:~1 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `board` DISABLE KEYS */;
+INSERT INTO `board` (`num`, `id`, `name`, `subject`, `content`, `regist_day`, `hit`, `ip`) VALUES
+	(1, 'pks4877', '박근수', 'ㅎㅇ', 'ㅎㅇㅎㅇ\r\nㅎㅇㅎㅇ', '2022-10-09 16:19:09', 3, '0:0:0:0:0:0:0:1');
 /*!40000 ALTER TABLE `board` ENABLE KEYS */;
 
 -- 테이블 bookmarketdb.book 구조 내보내기
@@ -51,13 +53,31 @@ CREATE TABLE IF NOT EXISTS `book` (
   PRIMARY KEY (`b_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- 테이블 데이터 bookmarketdb.book:~2 rows (대략적) 내보내기
+-- 테이블 데이터 bookmarketdb.book:~3 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
 INSERT INTO `book` (`b_id`, `b_name`, `b_unitPrice`, `b_author`, `b_description`, `b_publisher`, `b_category`, `b_unitsInStock`, `b_totalPages`, `b_releaseDate`, `b_condition`, `b_fileName`) VALUES
 	('ISBN1234', 'HTML5+CSS3', 15000, '황재호', '워드나 PPT 문서를 만들 수 있나요? 그러면 문제 없습니다. 지금 바로 웹페이지 제작에 도전해보세요. 지금 당장 컴퓨터가 없어도 괜찮습니다. 코드와 실행 화면이 바로 보여서 눈으로만 읽어도 어떻게 작동하는지 쉽게 파악할 수 있는 것은 기본이고, 중간중간 퀴즈를 추가하여 재미있게 게임하듯 복습할 수 있습니다.', '한빛미디어', 'Hello Coding', '1000', '288', '2018/03/02', 'New', 'ISBN1234.jpg'),
 	('ISBN1235', '쉽게 배우는 자바 프로그래밍', 27000, '우종중', '객체 지향의 핵심과 자바의 현대적 기능을 충실히 다루면서도초보자가 쉽게 학습할 수 있게 구성했습니다. 시각화 도구를 활용한 개념 설명과 군더더기 없는 핵심 코드를 통해 개념과 구현을 한 흐름으로 학습할 수 있습니다. 또한 ‘기초 체력을 다지는 예제 → 셀프 테스트 → 생각을 논리적으로 정리하며 한 단계씩 풀어 가는 도전 과제 → 스토리가 가미된 흥미로운 프로그래밍 문제’ 등을 통해 프로그래밍 실력을 차근차근 끌어올릴 수 있습니다', '한빛아카데미', 'IT모바일', '1000', '692', '2017/08/02', 'New', 'ISBN1235.jpg'),
 	('ISBN1236', '스프링4 입문 ', 27000, '하세가와 유이치 , 오오노 와타루 ', '스프링은 단순히 사용 방법만 익히는 것보다 아키텍처를 어떻게 이해하고 설계하는지가 더 중요합니다. 예제를 복사해 붙여넣는 식으로는 실제 개발에서 스프링을 제대로 활용할 수 없습니다. 이 책에서는 웹 애플리케이션의 기초를 다지고 스프링 코어를 살펴보며 클라우드 네이티브 입문까지 다룹니다. 이제 막 실무에 뛰어든 웹 애플리케이션 초급자나 개발 경험은 있지만 스프링은 사용해본 적 없는 분을 대상으로 가능한 한 쉽게 설명합니다', '한빛미디어', 'IT모바일', '1000', '520', '2017/11/01', 'New', 'ISBN1236.jpg');
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
+
+-- 테이블 bookmarketdb.galleryboard 구조 내보내기
+CREATE TABLE IF NOT EXISTS `galleryboard` (
+  `num` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `regist_day` varchar(30) DEFAULT NULL,
+  `hit` int DEFAULT NULL,
+  `ip` varchar(20) DEFAULT NULL,
+  `filename` varchar(50) DEFAULT NULL,
+  `fileseize` mediumtext,
+  PRIMARY KEY (`num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- 테이블 데이터 bookmarketdb.galleryboard:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `galleryboard` DISABLE KEYS */;
+/*!40000 ALTER TABLE `galleryboard` ENABLE KEYS */;
 
 -- 테이블 bookmarketdb.member 구조 내보내기
 CREATE TABLE IF NOT EXISTS `member` (
@@ -73,8 +93,10 @@ CREATE TABLE IF NOT EXISTS `member` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- 테이블 데이터 bookmarketdb.member:~0 rows (대략적) 내보내기
+-- 테이블 데이터 bookmarketdb.member:~1 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` (`id`, `password`, `name`, `gender`, `birth`, `mail`, `phone`, `address`, `regist_day`) VALUES
+	('pks4877', '1234', '박근수', NULL, '//', '@naver.com', '', '', '2022-10-09 15:53:26.789');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 
 
@@ -117,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `member` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- 테이블 데이터 webmarketdb.member:~3 rows (대략적) 내보내기
+-- 테이블 데이터 webmarketdb.member:~2 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
 INSERT INTO `member` (`id`, `password`, `name`, `gender`, `birth`, `mail`, `phone`, `address`, `regist_day`) VALUES
 	('admin', 'admin1234', 'admin', '남', '2000/10/13', 'geunsu902@gmail.com', '010-1234-5678', '경일게임아카데미', '2022-10-07 13:07:01.949'),
